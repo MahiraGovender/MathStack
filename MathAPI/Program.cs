555b13aaeb855c927eae0d27f56f996bc2373214
MahiraGovender_ST10438866 <ST10438866@vcconnect.edu.za>
2026-05-05 14:15:26 +0200

    Add GitHub Actions container pipeline for MathStack

--- MathAPI/Program.cs
+++ MathAPI/Program.cs
@@ -46,7 +46,10 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseAuthentication();
 app.UseAuthorization();
